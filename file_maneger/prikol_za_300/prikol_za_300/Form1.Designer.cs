bf6c896f8ee7851521e1797fa67b028942751e28
Alexandr <61242055+roflanDulya@users.noreply.github.com>
2020-03-06 18:33:11 +0300

    Maneger

---
+++ file_maneger/prikol_za_300/prikol_za_300/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace prikol_za_300
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(51, 117);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(75, 23);
+            this.metroButton1.TabIndex = 0;
+            this.metroButton1.Text = "Назад";
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroTextBox1
+            // 
+            this.metroTextBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.metroTextBox1.Location = new System.Drawing.Point(51, 60);
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.Size = new System.Drawing.Size(614, 23);
+            this.metroTextBox1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(51, 146);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(687, 238);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(663, 117);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(75, 23);
+            this.metroButton2.TabIndex = 3;
+            this.metroButton2.Text = "Далее";
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.metroTextBox1);
+            this.Controls.Add(this.metroButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+    }
+}
+
---
+++ file_maneger/prikol_za_300/prikol_za_300/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace prikol_za_300
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            if (metroTextBox1.Text[metroTextBox1.Text.Length - 1] == '\\')
+            {
+                metroTextBox1.Text = metroTextBox1.Text.Remove(metroTextBox1.Text.Length - 1, 1);
+                while (metroTextBox1.Text[metroTextBox1.Text.Length - 1] != '\\')
+                {
+                    metroTextBox1.Text = metroTextBox1.Text.Remove(metroTextBox1.Text.Length - 1, 1);
+                }
+            }
+            else if (metroTextBox1.Text[metroTextBox1.Text.Length - 1] != '\\')
+            {
+                while (metroTextBox1.Text[metroTextBox1.Text.Length - 1] != '\\')
+                {
+                    metroTextBox1.Text = metroTextBox1.Text.Remove(metroTextBox1.Text.Length - 1, 1);
+                }
+            }
+            listBox1.Items.Clear();
+            DirectoryInfo dir = new DirectoryInfo(metroTextBox1.Text);
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            foreach (DirectoryInfo crrDir in dirs)
+            {
+                listBox1.Items.Add(crrDir);
+            }
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo crrFile in files)
+            {
+                listBox1.Items.Add(crrFile);
+            }
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            DirectoryInfo dir = new DirectoryInfo(metroTextBox1.Text);
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            foreach (DirectoryInfo crrDir in dirs)
+            {
+                listBox1.Items.Add(crrDir);
+            }
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo crrFile in files)
+            {
+                listBox1.Items.Add(crrFile);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (Path.GetExtension(Path.Combine(metroTextBox1.Text, listBox1.SelectedItem.ToString())) == "")
+            {
+                metroTextBox1.Text = Path.Combine(metroTextBox1.Text, listBox1.SelectedItem.ToString());
+                listBox1.Items.Clear();
+                DirectoryInfo dir = new DirectoryInfo(metroTextBox1.Text);
+                DirectoryInfo[] dirs = dir.GetDirectories();
+                foreach (DirectoryInfo crrDir in dirs)
+                {
+                    listBox1.Items.Add(crrDir);
+                }
+                FileInfo[] files = dir.GetFiles();
+                foreach (FileInfo crrFile in files)
+                {
+                    listBox1.Items.Add(crrFile);
+                }
+            }
+            else
+            {
+                Process.Start(Path.Combine(metroTextBox1.Text, listBox1.SelectedItem.ToString()));
+            }
+        }
+    }
+}
