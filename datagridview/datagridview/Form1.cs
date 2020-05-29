16b7176ddee70ef66f6fc3c6c7e45df33ee8fcb7
Alexandr <61242055+roflanDulya@users.noreply.github.com>
2020-05-29 21:40:26 +0300

    DataGridView

---
+++ datagridview/datagridview/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+namespace datagridview
+{
+    public partial class Form1 : Form
+    {
+        
+        public string CmdText = "SELECT * FROM [table]";
+        public string ConnString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\\Users\\123\\Desktop\\1.mdb";
+        public Form1()
+        {
+            InitializeComponent();
+            OleDbDataAdapter dataAdapter = new OleDbDataAdapter(CmdText, ConnString);
+            DataSet ds = new DataSet();
+            dataAdapter.Fill(ds, "[table]");
+            dataGridView1.DataSource = ds.Tables[0].DefaultView;
+        }  
+    }
+}
+
